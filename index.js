29e1166bca12282e1ebaac7a94b7c2ec4a6bda86
Ash <ash@ashryanbeats.com>
2016-12-11 13:57:11 +0900

    Added comments and unified toggleKey function.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+/*
+	UI ELEMENTS
+*/
+// Containers
 var body = document.querySelector("body");
 var drumpadContainer = document.getElementById("drumpad-container");
-var drumpads = drumpadContainer.children;
 
+// Drumpads
+var drumpads = drumpadContainer.children;
 var keyA = document.getElementById("key-a");
 var keyS = document.getElementById("key-s");
 var keyD = document.getElementById("key-d");
@@ -12,8 +17,10 @@ var keyJ = document.getElementById("key-j");
 var keyK = document.getElementById("key-k");
 var keyL = document.getElementById("key-l");
 
+// Class for applying style
 var playingClass = " playing";
 
+// Audio
 var soundBoom = new Audio("sounds/boom.wav");
 var soundClap = new Audio("sounds/clap.wav");
 var soundHihat = new Audio("sounds/hihat.wav");
@@ -25,136 +32,140 @@ var soundTink = new Audio("sounds/tink.wav");
 var soundTom = new Audio("sounds/tom.wav");
 
 
+/*
+	EVENT INITIALIZATION
+*/
+// Add mouse handlers to drumpads
 for (var i = 0; i < drumpads.length; i++) {
 
-	(function attachClickHandler(i) {
+	(function attachMouseHandlers(i) {
 		var keyCode = parseInt(drumpads[i].dataset.key, 10);
-		drumpads[i].addEventListener("mousedown", function(){triggerKey(keyCode)}, false);
-		drumpads[i].addEventListener("mouseup", function(){untriggerKey(keyCode)});
+		drumpads[i].addEventListener("mousedown", function(){toggleKey(keyCode, true)}, false);
+		drumpads[i].addEventListener("mouseup", function(){toggleKey(keyCode, false)});
 	})(i);
 
 }
 
+// Trigger play on keyboard keydown
 body.onkeydown = function(e) {
 
 	if (!e.metaKey) {
 		e.preventDefault();
 
 		var keyCode = e.keyCode;
-
-		triggerKey(keyCode);
+		toggleKey(keyCode, true);
 	}
 }
 
-
+// Reset play status on keyboard keyup
 body.onkeyup = function(e) {
 
 	var keyCode = e.keyCode;
-
-	untriggerKey(keyCode);
+	toggleKey(keyCode, false);
 }
 
-function triggerKey(keyCode) {
 
-	console.log("triggerKey", keyCode);
+/*
+	HELPER FUNCTIONS
+*/
+// Logic for play handling
+function toggleKey(keyCode, play) {
 
 	switch (keyCode) {
 		case 65:
-			addPlayingClass(keyA);
-			playSound(soundBoom);
+			if (play) {
+				addPlayingClass(keyA);
+				playSound(soundBoom);
+			}
+			else {
+				removePlayingClass(keyA);
+			}
 			break;
 
 		case 83:
-			addPlayingClass(keyS);
-			playSound(soundClap);
+			if (play) {
+				addPlayingClass(keyS);
+				playSound(soundClap);
+			}
+			else {
+				removePlayingClass(keyS);
+			}
 			break;
 		
 		case 68:
-			addPlayingClass(keyD);
-			playSound(soundHihat);
+			if (play) {
+				addPlayingClass(keyD);
+				playSound(soundHihat);
+			}
+			else {
+				removePlayingClass(keyD);
+			}
 			break;
 		
 		case 70:
-			addPlayingClass(keyF);
-			playSound(soundKick);
+			if (play) {
+				addPlayingClass(keyF);
+				playSound(soundKick);
+			}
+			else {
+				removePlayingClass(keyF);
+			}
 			break;
 		
 		case 71:
-			addPlayingClass(keyG);
-			playSound(soundOpenhat);
+			if (play) {
+				addPlayingClass(keyG);
+				playSound(soundOpenhat);
+			}
+			else {
+				removePlayingClass(keyG);
+			}
 			break;
 		
 		case 72:
-			addPlayingClass(keyH);
-			playSound(soundRide);
+			if (play) {
+				addPlayingClass(keyH);
+				playSound(soundRide);
+			}
+			else {
+				removePlayingClass(keyH);
+			}
 			break;
 		
 		case 74:
-			addPlayingClass(keyJ);
-			playSound(soundSnare);
+			if (play) {
+				addPlayingClass(keyJ);
+				playSound(soundSnare);
+			}
+			else {
+				removePlayingClass(keyJ);
+			}
 			break;
 		
 		case 75:
-			addPlayingClass(keyK);
-			playSound(soundTink);
+			if (play) {
+				addPlayingClass(keyK);
+				playSound(soundTink);
+			}
+			else {
+				removePlayingClass(keyK);
+			}
 			break;
 
 		case 76:
-			addPlayingClass(keyL);
-			playSound(soundTom);
+			if (play) {
+				addPlayingClass(keyL);
+				playSound(soundTom);
+			}
+			else {
+				removePlayingClass(keyL);
+			}
 			break;
 
 		default:
 			break;
 	}
 }
-
-function untriggerKey(keyCode) {
-
-	console.log("untriggerKey", keyCode);
-
-	switch (keyCode) {
-		case 65:
-			removePlayingClass(keyA);
-			break;
-
-		case 83:
-			removePlayingClass(keyS);
-			break;
-		
-		case 68:
-			removePlayingClass(keyD);
-			break;
-		
-		case 70:
-			removePlayingClass(keyF);
-			break;
-		
-		case 71:
-			removePlayingClass(keyG);
-			break;
-		
-		case 72:
-			removePlayingClass(keyH);
-			break;
-		
-		case 74:
-			removePlayingClass(keyJ);
-			break;
-		
-		case 75:
-			removePlayingClass(keyK);
-			break;
-
-		case 76:
-			removePlayingClass(keyL);
-			break;
-
-		default:
-			break;
-	}
-}
-
 
 function addPlayingClass(key) {
 	key.className += playingClass;
@@ -163,6 +174,7 @@ function addPlayingClass(key) {
 function removePlayingClass(key) {
 	var classNames = key.className.split(" ");
 
+	// Removes all instances of playingClass that may have been added on long key presses
 	while (classNames.indexOf(playingClass.trim()) > 0) {
 		key.className = key.className.replace(playingClass, "");
 		classNames = key.className.split(" ");
